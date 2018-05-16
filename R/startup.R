058b1a694dbef7c069d83dd7276ae9e61f26aea3
Sasha D. Hafner <sasha.hafner@gmail.com>
2018-05-16 14:29:08 +0200

    Removed biogas-package Rd file and startup message file

--- R/startup.R
+++
@@ -1,7 +0,0 @@
-# Message on loading
-
-.onAttach <- function(libname, pkgname) {
-  packageStartupMessage('biogas v 1.10.0.\nFor updates, join the biogas mailing list--send a message to sasha.hafner@eng.au.dk.\nFor a web app interface to the biogas package, browse to\nhttps://biotransformers.shinyapps.io/oba1/.')
-}
-
-
