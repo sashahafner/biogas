31493801e431cc94526652818155551474d6061c
Sasha D. Hafner <sasha.hafner@gmail.com>
2018-12-19 15:16:26 +0100

    Add f1 method to interp for manometric cumBg with mixed vent & no vent

--- R/interp.R
+++ R/interp.R
@@ -1,19 +1,19 @@
-# Modified: 12 May 2016 SDH
 
-interp <-
-function(times, y, time.out, method = 'linear', extrap = FALSE) {
+interp <- function(times, y, time.out, method = 'linear', extrap = FALSE) {
 
   # Check arguments
   checkArgClassValue(times, c('numeric', 'integer', 'POSIXct', 'POSIXlt', 'POSIXt', 'POSIX', 'difftime'))
   checkArgClassValue(y, c('numeric', 'integer'))
   checkArgClassValue(time.out, c('numeric', 'integer', 'POSIXct', 'POSIXlt', 'POSIXt', 'POSIX'))
-  checkArgClassValue(method, c('character'), expected.values = c('linear', 'natural', 'hyman', 'fmm'))
+  checkArgClassValue(method, c('character'), expected.values = c('linear', 'natural', 'hyman', 'fmm', 'f1'))
   checkArgClassValue(extrap, 'logical')
 
   #if(extrap & (max(time.out)>max(times) | min(time.out)<min(times))) warning('Some results are extrapolated. Set extrap = FALSE to prevent this.')
 
   if(method=='linear') {
     y.out <- approx(times, y, xout = time.out, rule = ifelse(extrap, 2, 1))$y
+  } else if(tolower(method) == 'f1') { 
+    y.out <- approx(times, y, xout = time.out, rule = ifelse(extrap, 2, 1), f = 1)$y
   } else { 
     # Or for natural, hyman, or fmm methods (others will return an error)
     y.out <- spline(times, y, xout = time.out, method = method)$y
