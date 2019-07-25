0cdb3aab98787ee55b03dcec683d126b9da49b23
Nanna Løjborg <nannalojborg@hotmail.com>
2019-07-25 16:39:08 +0200

    test
    Test

---
+++ R/xx.R
@@ -0,0 +1,15 @@
+# Nanna Løjborg
+x <- 10
+y <- 8
+ls()
+rm(x,y)
+rm(list = ls())
+x <- y <- z <- 1
+a <- 50
+a >= 100 | a == 50
+a > 0 & a < 100
+sqrt(10)
+x <- rnorm(1000)
+plot(1:10)
+methods(plot)
+methods(summary)
