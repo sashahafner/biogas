c1ff111ce692b4eb156dfa156c5d820a8fd56b88
Sasha D. Hafner <sasha@hafnerconsulting.com>
2024-12-04 06:00:35 -0500

    fix name errors and more.
    Use UQGravBiogas not Mass.
    Start #46, at least mention new functions and vignettes.

--- make-data/make_data.R
+++ make-data/make_data.R
@@ -1,7 +1,7 @@
 # Create Rda files
 
 UQGravSetup <- read.csv('UQ_grav_setup.csv')
-UQGravMass <- read.csv('UQ_grav_mass.csv')
+UQGravBiogas <- read.csv('UQ_grav_mass.csv')
 
 save(UQGravSetup, file = '../data/UQGravSetup.rda')
-save(UQGravMass, file = '../data/UQGravMass.rda')
+save(UQGravBiogas, file = '../data/UQGravBiogas.rda')
